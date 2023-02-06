e520295208a3ac5ca9cedc3bad05494520c05e96
Jorge Enrique Mojica Martinez <jmojica@skandia.com.co>
2023-02-06 14:58:07 -0500

    cambio mensaje bienvenida

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Bienvenido al api raffle." });
 });
 
 require("./app/routes")(app);
